cf808bef54c17c267ba55009303bbbdc6c42afe7
Tenno641 <ahmedfotoh2003@gmail.com>
2023-10-20 19:06:27 +0300

    Activity_main / Login page

--- app/src/main/java/com/albernameg/wanderers/MainActivity.java
+++ app/src/main/java/com/albernameg/wanderers/MainActivity.java
@@ -2,13 +2,26 @@ package com.albernameg.wanderers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button explore_btn;
+    Intent loginIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        explore_btn = findViewById(R.id.explore_btn);
+        loginIntent = new Intent(this, LoginActivity.class);
+
+        explore_btn.setOnClickListener(v -> {
+            startActivity(loginIntent);
+        });
+
     }
 }
